01f072988ad5f8f44f13782fb84267cca565dae8
Vinny-Ag <54929790+Vinny-Ag@users.noreply.github.com>
2021-02-15 21:37:25 -0800

    Add files via upload

---
+++ prgrm_02_03.f90
@@ -0,0 +1,90 @@
+		program prgrm_02_03
+		implicit none
+		integer::n1,n2
+		real::M,L,K
+		real::eigenvalue,eigenvalueModified,eigenvalueModTotal
+		real::PIB_1D_T_ELEMENT,PIB_1D_MODIFIED_V_ELEMENT,PIB_1D_MODIFIED_HAMILTONIAN_ELEMENT
+		
+		write(*,*)' Input PIB parameters:'
+		read(*,*)M,L,n1,n2,K
+		
+		if (M*L.eq.0) then
+			write(*,*)'Error, PIB program requires M & L be nonzero integers'
+		endif
+		
+		eigenvalue = PIB_1D_T_ELEMENT(M,L,n1,n2)
+		eigenvalueModified = PIB_1D_MODIFIED_V_ELEMENT(M,L,n1,n2,K)
+		eigenvalueModTotal = PIB_1D_MODIFIED_HAMILTONIAN_ELEMENT(M,L,n1,n2,K)
+		
+	999 format(1x,'Kinetic energy matrix element   ',I5,',',I5,' is ',F12.5,'.')
+	998 format(1x,'Potential energy matrix element ',I5,',',I5,' is ',F12.5,'.')
+	997 format(1x,'Hamiltonian matrix element      ',I5,',',I5,' is ',F12.5,'.')
+		
+		
+		print 999,1,1,eigenvalue
+		print 998,1,1,eigenvalueModified
+		print 997,1,1,eigenvalueModTotal
+		
+		end program prgrm_02_03
+		
+		
+		!********end of main driver, function following*******
+		
+		
+		Real function PIB_1D_T_ELEMENT(A,B,C,D)
+		implicit none
+		integer::C,D
+		real::A,B,val01
+		
+		write(*,*)'getting eigenvalues!'
+		
+		
+		val01= ((C**2)+(D**2))/(A*(B**2))
+		
+		if (D.ne.0) then
+			write(*,*)'printing n2 eigenvalue: '
+		else
+			write(*,*)'printing n1 eigenvalue: '
+		endif
+		
+		
+		PIB_1D_T_ELEMENT= val01
+		
+		
+		return
+		end
+		
+		
+		! ********the modified PIB energy function begins below*******
+		
+		
+		Real function PIB_1D_MODIFIED_V_ELEMENT(A,B,C,D,K)
+		implicit none
+		integer::C,D
+		real::A,B,val02,K
+		
+		val02 = K*(B**2)/4
+		
+		PIB_1D_MODIFIED_V_ELEMENT = val02
+		
+		
+		return
+		end
+		
+		
+		! ********rotal modified matrix element for PIB below*******
+		
+		
+		Real function PIB_1D_MODIFIED_HAMILTONIAN_ELEMENT(A,B,C,D,K)
+		implicit none
+		integer::C,D
+		real::A,B,val03,K
+		
+		val03 = ((C**2)+(D**2))/(A*(B**2))+((K*B**2)/4)
+		
+		PIB_1D_MODIFIED_HAMILTONIAN_ELEMENT = val03
+		
+		
+		return
+		end
+		
